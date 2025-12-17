c00faaf5728fe4443d3066cba93d095e726198c9
diego538 <spotykajloinnokentij@gmail.com>
2025-12-17 17:28:51 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -5,9 +5,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 if not BOT_TOKEN:
-    raise ValueError("❌ BOT_TOKEN не найден в переменных окружения BotHost!")
+    raise ValueError("❌ BOT_TOKEN не найден в переменных окружения")
 
 if not CHAT_ID:
-    raise ValueError("❌ CHAT_ID не найден в переменных окружения BotHost!")
+    raise ValueError("❌ CHAT_ID не найден в переменных окружения")
 
 bot = telebot.TeleBot(BOT_TOKEN)
